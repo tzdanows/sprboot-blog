9c23a0ba3a67c70834add5303c32c6c0f348b3d5
tzdanows <tom.zdan9850@gmail.com>
2024-02-27 14:16:31 -0600

    initial commit of stable app infra

---
+++ src/main/java/tzdanows/monoreposprboot/MonorepoSprbootApplication.java
@@ -0,0 +1,13 @@
+package tzdanows.monoreposprboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonorepoSprbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonorepoSprbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tzdanows/monoreposprboot/MonorepoSprbootApplicationTests.java
@@ -0,0 +1,13 @@
+package tzdanows.monoreposprboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonorepoSprbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
